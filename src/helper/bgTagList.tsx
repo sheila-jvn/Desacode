fb739e52f179718f41593a99d96ae6779fd24d1c
Sheila Putri Javana <jvnsheila@gmail.com>
2024-05-17 09:52:02 +0700

    feat: tag card categories

--- src/helper/bgTagList.tsx
+++ src/helper/bgTagList.tsx
@@ -18,3 +18,31 @@ export function determineTagColor(tag: string) {
 
   return color;
 }
+
+export function determineTagColor2(title: string) {
+  let color = "bg-spring-blue"; // Default spring blue color
+  console.log(title)
+
+  switch (title){
+    case "DesaPOS":
+      color = "bg-[#FF69B4]"; // Hot Pink
+      break;
+    case "SolveEase TradeStream":
+      color = "bg-[#32CD32]"; // Lime Green
+      break;
+    case "SIMEDEASE":
+      color = "bg-[#1E90FF]"; // Dodger Blue
+      break;
+    case "Smartmedic+":
+      color = "bg-[#00BFFF]"; // Deep Sky Blue
+      break;
+    case "Solve Loyaltease":
+      color = "bg-[#FF8C00]"; // Dark Orange
+      break;
+    default:
+      color = "bg-[#00BFFF]"; // Deep Sky Blue
+      break;
+  }
+
+  return color;
+}
